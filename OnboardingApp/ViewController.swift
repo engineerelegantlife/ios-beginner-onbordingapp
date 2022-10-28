5572bc76aa63b39d6e3ed23493b568f6f8707b8d
Tomo Shimizu <tomoshimizu@TomoMac.local>
2022-10-28 16:56:13 +0900

    first commit

---
+++ OnboardingApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OnboardingApp
+//
+//  Created by Tomo Shimizu on 2022/10/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
